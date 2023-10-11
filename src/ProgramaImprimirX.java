9860c458aa91925f3d4326a672f886578fb7ff93
Luis Alfonso <alfonso97isc@gmail.com>
2023-10-10 18:05:24 -0600

    ejemplos

--- src/ProgramaImprimirX.java
+++ src/ProgramaImprimirX.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class ProgramaImprimirX {
+import java.util.Scanner;
+
+public class ProgramaImprimirX {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Ingrese el tamaño de la matriz (nxn): ");
+        int n = s.nextInt();
+
+        if (n == 0){
+            System.out.println("Error");
+            System.exit(1);
+        }
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; i++){
+                if (i == j || (j == n - i - 1)){
+                    System.out.println("X");
+                }else {
+                    System.out.println("_");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
